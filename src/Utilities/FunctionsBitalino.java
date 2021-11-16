19f8ed6a46ee7764018fbcd5635fddce6113b25d
carmen <carmen@192.168.1.53>
2021-11-16 21:21:07 +0100

    No habia guardado una cosilla

--- src/Utilities/FunctionsBitalino.java
+++ src/Utilities/FunctionsBitalino.java
@@ -29,45 +29,6 @@ public class FunctionsBitalino {
         }
         return error;
     }*/
-    public static boolean[] introducePatient(String name, String username, String email, String adress, String phone, char[] password) {
-        boolean[] error = null;
-
-        if (!Exceptions.checkString(name)) {
-            error[0] = true;
-        } else {
-            error[0] = false;
-        }
-
-        if (!Exceptions.checkUsername(username)) {
-            error[1] = true;
-        } else {
-            error[1] = false;
-        }
-
-        if (!Exceptions.checkEmail(email)) {
-            error[2] = true;
-        } else {
-            error[2] = false;
-        }
-        if (!Exceptions.checkPhone(phone)) {
-            error[3] = true;
-        } else {
-            error[3] = false;
-        }
-        if (!error[0] && !error[1] && !error[2] && !error[3]) {
-            PatientPrincipalWindow.patient.setFullName(name);//guardar en base de datos
-            PatientPrincipalWindow.patient.setUsername(username);
-            PatientPrincipalWindow.patient.setAddress(adress);
-            PatientPrincipalWindow.patient.setEmail(email);
-            PatientPrincipalWindow.patient.setPhonenumber(phone);
-            //PatientPrincipalWindow.patient.setPassword(password);
-            PatientPrincipalWindow.patientManager.createPatient(PatientPrincipalWindow.patient);
-
-        }
-        return error;
-        //lo del hash con las pass
-    }
-
     public static int getSampling(int i) {
         int sampling = 0;
         switch (i) {
--- test/db/sql/SQLPatientManagerTest.java
+++ test/db/sql/SQLPatientManagerTest.java
@@ -8,32 +8,32 @@ package db.sql;
 import Pojos.Patient;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
  * @author Cristina
  */
 public class SQLPatientManagerTest {
-    
+
     public SQLPatientManagerTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -85,9 +85,9 @@ public class SQLPatientManagerTest {
         System.out.println("readPatient");
         Integer id = null;
         SQLPatientManager instance = null;
-        instance.readPatient(id);
+        instance.getPatientById(id);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-    
+
 }
